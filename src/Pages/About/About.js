19e4f6901ae89d81a35a383a7e9e30c34249838c
zannatreya <zannatulbintabahar@yahoo.com>
2022-04-18 12:42:09 +0600

    about route done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -11,7 +11,13 @@ const About = () => {
             </div>
             <div>
                 <h4>Name: Zannatul Binta Bahar</h4>
-                <h5>Goal: </h5>
+                <h5>Goal:I want to become a freelance/remote website/app developer.
+                    I absolutely hated the routine life and other things related to a 9–5 job.
+                    Currently I'm working on revising my JavaScript skills.
+                    I have worked with HTML and CSS for about 1 years.
+                    Now I want to work on JavaScript more as there are better growth opportunities.
+                    Planning on doing a certification in MERN stack so I can get into full stack development
+                    which I believe is going to be a great career choice for the future. </h5>
             </div>
         </div>
     );
